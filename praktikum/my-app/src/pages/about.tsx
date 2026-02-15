f33c10f7df64378e6cfb9b3bb8c26a82b38bfaf9
olthapage <olthar.rosyeda01@gmail.com>
2026-02-15 16:26:19 +0700

    Praktikum_Tugas 1

---
+++ praktikum/my-app/src/pages/about.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+
+      <div>
+        <h1>Halaman About</h1>
+        <hr />
+
+        <p><strong>Nama Mahasiswa:</strong> Oltha Rosyeda Al'haq</p>
+        <p><strong>NIM:</strong> 2341720145</p>
+        <p><strong>Program Studi:</strong> D4 Teknik Informatika</p>
+      </div>
+    </>
+  )
+}
