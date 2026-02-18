90499657f104ff09b0bb8c07b3379eb915632cf0
olthapage <olthar.rosyeda01@gmail.com>
2026-02-18 08:33:17 +0700

    Praktikum2

---
+++ praktikum2/my-app/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Praktikum Next.js Pages Router</h1>
+        <p>Mahasiswa D4 Pengembangan Web</p>
+
+        <br />
+
+        <Link href="/about">
+          <button>Halaman About</button>
+        </Link>
+    </div>
+  )
+}
