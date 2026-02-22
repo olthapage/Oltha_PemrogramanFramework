0b3c560b36d1d07a2b5aa8436d882343677dc31a
olthapage <olthar.rosyeda01@gmail.com>
2026-02-22 11:01:23 +0700

    Langkah3_Pengujian Catch-All Route_Menggunakan yang segment nya dibatasi

--- praktikum3/my-app/src/pages/shop/[...slug].tsx
+++ praktikum3/my-app/src/pages/shop/[...slug].tsx
@@ -4,14 +4,10 @@ const halamanToko = () => {
   // const Router = useRouter();
   // console.log(Router);
   const { query } = useRouter();
-
   return (
     <div>
       <h1>Halaman Toko</h1>
-      {/* <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> menggunakan backtick bukan petik satu */}
-      <p>
-        Toko: {Array.isArray(query.slug) ? query.slug.join("-") : query.slug}
-      </p>
+      <p>Toko: {`${query.slug && query.slug[0]+"-"+ query.slug[1]}`}</p> {/* menggunakan backtick bukan petik satu*/}
     </div>
   );
 };
