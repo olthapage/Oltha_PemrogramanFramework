ca9d16a8c83d848ab02c615bc8e0fcf1969c3f9d
olthapage <olthar.rosyeda01@gmail.com>
2026-02-27 21:53:15 +0700

    TugasPraktikum_Tugas 2

--- praktikum4/my-app/src/pages/produk/index.tsx
+++ praktikum4/my-app/src/pages/produk/index.tsx
@@ -1,19 +1,7 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import ProdukView from "../views/produk";
 
-const produk = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const { push } = useRouter();
-
-  useEffect(() => {
-    if (!isLogin) {
-      push("/auth/login");
-    }
-  }, []);
-
-  return (
-    <div>Produk User Page</div>
-  );
+const ProdukPage = () => {
+  return <ProdukView />;
 };
 
-export default produk;
+export default ProdukPage;
