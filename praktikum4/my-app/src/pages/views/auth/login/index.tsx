5d30fd9af102586af3774c0c5bc6f038a5f04636
olthapage <olthar.rosyeda01@gmail.com>
2026-02-27 22:15:29 +0700

    TugasPraktikum_Tugas3

--- praktikum4/my-app/src/pages/views/auth/login/index.tsx
+++ praktikum4/my-app/src/pages/views/auth/login/index.tsx
@@ -11,12 +11,28 @@ const TampilanLogin = () => {
         push('/produk');
     }
     return (
-        <div className={styles.login}>
-            <h1 className="text-3xl font-bold text-blue-600 ">Halaman Login</h1>
-            <button onClick={() => handleLogin()}>Login</button> <br />
-            <h1 style={{color:"red",border:"1px solid red",borderRadius:"5px",padding:"5px"}}> belum punya akun</h1>
-            <Link href={"/auth/register"}>Ke Halaman Register</Link>
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
+      
+      <h1 className="text-3xl font-bold text-blue-600">
+        Halaman Login
+      </h1>
+
+      <button onClick={handleLogin}>
+        Login
+      </button>
+
+      <h2 className="text-red-600 border border-red-600 rounded-md px-3 py-1">
+        Belum punya akun
+      </h2>
+
+      <Link
+        href="/auth/register"
+        className="text-blue-500 hover:text-blue-700 underline"
+      >
+        Ke Halaman Register
+      </Link>
+
+    </div>
     );
 }
 
