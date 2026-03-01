2c8aabcd466c4b61ad1b4dd19f2e092985402cd7
olthapage <olthar.rosyeda01@gmail.com>
2026-03-01 22:56:33 +0700

    langkah8_Konfigurasi Firebase

--- praktikum6/my-app/src/pages/utlis/db/firebase.ts
+++ praktikum6/my-app/src/pages/utlis/db/firebase.ts
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDWnncQTGjKUnpFeHWZk22X4hLxkzcCYPg",
-  authDomain: "framework-next-f0a93.firebaseapp.com",
-  projectId: "framework-next-f0a93",
-  storageBucket: "framework-next-f0a93.firebasestorage.app",
-  messagingSenderId: "675920287066",
-  appId: "1:675920287066:web:93b18a6f799183781a4f63",
-  measurementId: "G-41WRY2X0W6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export default app;
