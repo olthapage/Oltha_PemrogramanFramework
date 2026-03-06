f2bd9d09dba89ad3264dd3d9d030109abc980a72
olthapage <olthar.rosyeda01@gmail.com>
2026-03-06 21:31:12 +0700

    Praikum 9_Jobsheet 10

---
+++ praktikum9/my-app/src/pages/404.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "@/styles/404.module.scss";
+
+const Custom404 = () => {
+  return (
+    <>
+      <Head>
+        <title>404 - Halaman Tidak Ditemukan</title>
+        <meta
+          name="description"
+          content="Halaman yang Anda cari tidak tersedia atau sudah dipindahkan."
+        />
+      </Head>
+
+      <div className={styles.error}>
+        <img
+          src="/page-not-found.png"
+          alt="404"
+          className={styles.error__image}
+        />
+
+        <h1 className={styles.error__title}>
+          404 - Halaman Tidak Ditemukan
+        </h1>
+
+        <p className={styles.error__desc}>
+          Maaf, halaman yang Anda cari tidak tersedia atau mungkin telah dipindahkan.
+        </p>
+
+        <Link href="/" className={styles.error__button}>
+          Kembali ke Home
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Custom404;
