6e4c30bc3ad54d47d438508f633850ad38e2afb4
olthapage <olthar.rosyeda01@gmail.com>
2026-03-06 11:10:55 +0700

    Bagian 2_Implementasi getServerSideProps pada server.tsx

--- praktikum8/my-app/src/pages/produk/server.tsx
+++ praktikum8/my-app/src/pages/produk/server.tsx
@@ -1,12 +1,28 @@
 import TampilanProduk from "../views/produk";
+import { ProductType } from "../types/product.type";
 
-const halamanProdukServer = () => {
+const halamanProdukServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
   return (
     <div>
       <h1>Halaman Produk Server</h1>
-      <TampilanProduk products={[]} isLoading/>
+      <TampilanProduk products={products} isLoading={false} />
     </div>
   );
 };
 
 export default halamanProdukServer;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/produk");
+  const respone = await res.json();
+  
+  // console.log("Data produk yang diambil dari API:", respone);
+  
+  return {
+    props: {
+      products: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
--- praktikum8/my-app/src/pages/views/produk/index.tsx
+++ praktikum8/my-app/src/pages/views/produk/index.tsx
@@ -8,41 +8,54 @@ type ProductType = {
   category: string;
 };
 
-const ProdukView = ({ products,isLoading,}: {  products: ProductType[];isLoading: boolean;}) => {
+const TampilanProduk = ({ products, isLoading, }: { products: ProductType[]; isLoading?: boolean }) => {
   return (
     <div className={styles.produk}>
       <h1 className={styles.produk__title}>Daftar Produk</h1>
+
       <div className={styles.produk__content}>
-        {isLoading
-          ? Array.from({ length: 4 }).map((_, index) => (
-              <div key={index} className={styles.produk__content__skeleton}>
-                <div className={styles.produk__content__skeleton__image}></div>
-                <div className={styles.produk__content__skeleton__name}></div>
-                <div
-                  className={styles.produk__content__skeleton__category}
-                ></div>
-                <div className={styles.produk__content__skeleton__price}></div>
+
+        {isLoading ? (
+          <>
+            <div className={styles.produk__content__skeleton}>
+              <div className={styles.produk__content__skeleton__image}></div>
+              <div className={styles.produk__content__skeleton__name}></div>
+              <div className={styles.produk__content__skeleton__category}></div>
+              <div className={styles.produk__content__skeleton__price}></div>
+            </div>
+          </>
+        ) : (
+          products.map((product: ProductType) => (
+            <div
+              key={product.id}
+              className={styles.produk__content__item}
+            >
+              <div className={styles.produk__content__item__image}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  width={200}
+                />
               </div>
-            ))
-          : products.map((product: ProductType) => (
-              <div key={product.id} className={styles.produk__content__item}>
-                <div className={styles.produk__content__item__image}>
-                  <img src={product.image} alt={product.name} width={200} />
-                </div>
-                <h2 className={styles.produk__content__item__name}>
-                  {product.name}
-                </h2>
-                <p className={styles.produk__content__item__category}>
-                  Kategori: {product.category}
-                </p>
-                <p className={styles.produk__content__item__price}>
-                  Rp {product.price.toLocaleString("id-ID")}
-                </p>
-              </div>
-            ))}
+
+              <h4 className={styles.produk__content__item__name}>
+                {product.name}
+              </h4>
+
+              <p className={styles.produk__content__item__category}>
+                {product.category}
+              </p>
+
+              <p className={styles.produk__content__item__price}>
+                Rp {product.price.toLocaleString("id-ID")}
+              </p>
+            </div>
+          ))
+        )}
+
       </div>
     </div>
   );
 };
 
-export default ProdukView;
+export default TampilanProduk;
