0407e4329f0a06904cdd1e46225d17031010a502
olthapage <olthar.rosyeda01@gmail.com>
2026-03-07 22:34:06 +0700

    Bagian 1_Membuat Dynamic Route

--- praktikum10/my-app/src/pages/produk/index.tsx
+++ praktikum10/my-app/src/pages/produk/index.tsx
@@ -1,17 +1,53 @@
-import ProdukView from "../../views/produk";
-import { use, useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import fetcher from "../../utils/swr/fetcher";
+import styles from "./product.module.scss";
+import Link from "next/link";
 
-const halamanProduk = () => {
-  const [products, setProducts] = useState([]);
-  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
-    return (
-      <div>
-        <ProdukView products={data?.data || []} isLoading={isLoading} />
-      </div>
-    );
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
 };
 
-export default halamanProduk;
+const TampilanProduk = ({ products = [] }: { products?: ProductType[] }) => {
+  return (
+    <div className={styles.produk}>
+      <h1 className={styles.produk__title}>Daftar Produk</h1>
+      <div className={styles.produk__content}>
+        {products.length > 0 ? (
+          <>
+            {products.map((product: ProductType) => (
+              <Link
+                href={`/produk/${product.id}`}
+                key={product.id}
+                className={styles.produk__content__item}
+              >
+                <div className={styles.produk__content__item__image}>
+                  <img src={product.image} alt={product.name} width={200} />
+                </div>
+                <h4 className={styles.produk__content__item__name}>
+                  {product.name}
+                </h4>
+                <p className={styles.produk__content__item__category}>
+                  {product.category}
+                </p>
+                <p className={styles.produk__content__item__price}>
+                  Rp {product.price.toLocaleString("id-ID")}
+                </p>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <div className={styles.produk__content__skeleton}>
+            <div className={styles.produk__content__skeleton__image}></div>
+            <div className={styles.produk__content__skeleton__name}></div>
+            <div className={styles.produk__content__skeleton__category}></div>
+            <div className={styles.produk__content__skeleton__price}></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TampilanProduk;
