85183bc202a09434f3edb674559f9b77c1ee4aa3
olthapage <olthar.rosyeda01@gmail.com>
2026-03-09 12:23:02 +0700

    Bagian 3_Implementasi SSR

--- praktikum10/my-app/src/pages/produk/[produk].tsx
+++ praktikum10/my-app/src/pages/produk/[produk].tsx
@@ -2,30 +2,44 @@ import fetcher from "@/utils/swr/fetcher";
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import DetailProduk from "../../views/DetailProduct";
+import { ProductType } from "../../types/product.type";
 
-const HalamanProduk = () => {
-  const router = useRouter();
-  const { produk } = router.query;
-
-  const { data, error, isLoading } = useSWR( produk ? `/api/produk/${produk}` : null,  fetcher,);
-
-  if (!router.isReady) {
-    return <p>Loading router...</p>;
-  }
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (!data || !data.data) {
-    return <p>Data tidak ditemukan</p>;
-  }
+const HalamanProduk = ({ product }: { product: ProductType }) => {
+  /* digunakan client-side rendering
+  // const Router = useRouter();
+  // // console.log(Router);
+  // const { query } = useRouter();
+  // const { data, error, isLoading } = useSWR(
+  //   `/api/products/${query.produk}`,
+  //   fetcher,
+  // );
+  // return (
+  //   <div>
+  //     <DetailProduk products={isLoading ? [] : data.data} />
+  //   </div>
+  // );
+  */
 
   return (
     <div>
-      <DetailProduk products={data.data} />
+      <DetailProduk products={product} />
     </div>
   );
 };
 
 export default HalamanProduk;
+
+// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
+// {digunakan server-side rendering}
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  
+  console.log("Data produk yang diambil dari API:", respone);
+
+  return {
+    props: {
+      product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
