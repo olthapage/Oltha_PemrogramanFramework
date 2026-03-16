98eae7f8b64f6fbb1d9e44f44604d3dbd9106105
olthapage <olthar.rosyeda01@gmail.com>
2026-03-16 15:42:15 +0800

    Implementasi ISR Otomatis

--- praktikum11/my-app/src/pages/produk/static.tsx
+++ praktikum11/my-app/src/pages/produk/static.tsx
@@ -14,14 +14,15 @@ const halamanProdukStatic = (props: { products: ProductType[] }) => {
 export default halamanProdukStatic;
 
 export async function getStaticProps() {
-  const res = await fetch("http://127.0.0.1:3000/api/produk");
-  // const response = ProductType[] = await res.json();
+  const res = await fetch('http://127.0.0.1:3000/api/produk');
+  // const response: ProductType[] = await res.json();
   const response: { data: ProductType[] } = await res.json();
 
-  //console.log("Data Produk yang diambil dari API:", response);
+  // console.log("Data produk yang diambil dari API:", response);
   return {
     props: {
       products: response.data,
     },
+    revalidate: 10, // Revalidate data setiap 10 detik
   };
 }
