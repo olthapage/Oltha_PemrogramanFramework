7e523edc91a48e7f7037f119a57ef4514279584e
olthapage <olthar.rosyeda01@gmail.com>
2026-04-02 17:12:18 +0700

    JS15_praktikum_bagian 2

--- praktikum14/my-app/src/utils/db/servicefirebase.ts
+++ praktikum14/my-app/src/utils/db/servicefirebase.ts
@@ -1,20 +1,53 @@
-import {getFirestore, collection, getDocs,Firestore, getDoc, doc} from 'firebase/firestore';
+import {getFirestore, collection, getDocs,Firestore, getDoc, doc, query, addDoc, where,} from 'firebase/firestore';
 import app from "./firebase";
-import { snapshot } from 'node:test';
+import { stat } from 'node:fs';
 
 const db = getFirestore(app);
 
-export async function retrieveProducts(collectionName: string) {
-  const snapshot = await getDocs(collection(db, collectionName));
-  const data = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  
-  return data;
+export async function getData(collectionName: string) {
+    const snapshot = await getDocs(collection(db, collectionName));
+    const data = snapshot.docs.map(doc =>
+         ({ id: doc.id, 
+            ...doc.data()
+         }));
+    return data;
 }
+
 export async function retrieveDataByID(collectionName: string, id: string) {
     const snapshot = await getDoc(doc(db, collectionName, id));
     const data = snapshot.data();
     return data;
+}
+
+export async function signUp(
+    userData: {
+        email: string;
+        fullName: string;
+        password: string;
+    },
+    callback: Function,
+) {
+    const q =query(collection(db, 'users'), 
+    where('email', '==', userData.email),
+);
+const querySnapshot = await getDocs(q);
+const data = querySnapshot.docs.map(doc => ({
+     id: doc.id,
+      ...doc.data(),
+    }));
+    //console.log("Query result:", data);
+    if (data.length === 0) {
+        //user belum ada -> boleh buat akun baru
+        // await addDoc(collection(db, 'users'), userData);
+        //console.log("User registered:", userData);
+        callback({
+            status: 'success',
+            message: 'User registered successfully',
+        });
+    } else {
+        callback({
+            status: 'error',
+            message: 'User already exists',
+        })
+    }
 }
--- praktikum14/my-app/src/views/auth/register/index.tsx
+++ praktikum14/my-app/src/views/auth/register/index.tsx
@@ -1,12 +1,48 @@
 import Link from "next/link";
 import style from "./register.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const TampilanRegister = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formdata = new FormData(event.currentTarget);
+    const email = formdata.get("email") as string;
+    const fullName = formdata.get("Fullname") as string;
+    const password = formdata.get("Password") as string;
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email,fullName, password}),
+    });
+    // const result = await response.json();
+    // console.log(result);
+    if (response.status === 200) {
+      form.reset();
+      //event.currentTarget.reset();
+      setIsLoading(false);
+      push("/auth/login");
+    } else {
+      setIsLoading(false);
+      setError(
+        response.status === 400
+          ? "User already exists"
+          : "An error occurred. Please try again later."
+      );
+    }
+  };
   return (
     <div className={style.register}>
       <h1 className={style.register__title}>Halaman Register</h1>
       <div className={style.register__form}>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className={style.register__form__item}>
             <label
               htmlFor="email"
