9bc201c226551f50822f0a21a43e90e824b3f77c
olthapage <olthar.rosyeda01@gmail.com>
2026-04-02 18:17:02 +0700

    JS15_praktikum_bagian3

--- praktikum14/my-app/src/utils/db/servicefirebase.ts
+++ praktikum14/my-app/src/utils/db/servicefirebase.ts
@@ -1,53 +1,64 @@
-import {getFirestore, collection, getDocs,Firestore, getDoc, doc, query, addDoc, where,} from 'firebase/firestore';
+import {getFirestore,collection,getDocs,Firestore,
+        getDoc,doc,query,addDoc,where,
+} from "firebase/firestore";
 import app from "./firebase";
-import { stat } from 'node:fs';
+import bcrypt from "bcrypt";
 
 const db = getFirestore(app);
 
 export async function getData(collectionName: string) {
-    const snapshot = await getDocs(collection(db, collectionName));
-    const data = snapshot.docs.map(doc =>
-         ({ id: doc.id, 
-            ...doc.data()
-         }));
-    return data;
+  const snapshot = await getDocs(collection(db, collectionName));
+  const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  return data;
 }
 
 export async function retrieveDataByID(collectionName: string, id: string) {
-    const snapshot = await getDoc(doc(db, collectionName, id));
-    const data = snapshot.data();
-    return data;
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
+  return data;
 }
 
 export async function signUp(
-    userData: {
-        email: string;
-        fullName: string;
-        password: string;
-    },
-    callback: Function,
+  userData: {
+    email: string;
+    fullName: string;
+    password: string;
+    role?: string;
+  },
+  callback: Function,
 ) {
-    const q =query(collection(db, 'users'), 
-    where('email', '==', userData.email),
-);
-const querySnapshot = await getDocs(q);
-const data = querySnapshot.docs.map(doc => ({
-     id: doc.id,
-      ...doc.data(),
-    }));
-    //console.log("Query result:", data);
-    if (data.length === 0) {
-        //user belum ada -> boleh buat akun baru
-        // await addDoc(collection(db, 'users'), userData);
-        //console.log("User registered:", userData);
-        callback({
-            status: 'success',
-            message: 'User registered successfully',
-        });
-    } else {
-        callback({
-            status: 'error',
-            message: 'User already exists',
-        })
-    }
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  //console.log("Query result:", data);
+  if (data.length > 0) {
+    //user belum ada -> boleh buat akun baru
+    // await addDoc(collection(db, 'users'), userData);
+    //console.log("User registered:", userData);
+     callback({
+    status: "error",
+    message: "User already exists",
+  });
+  } else {
+    userData.password = await bcrypt.hash(userData.password, 10);
+    userData.role = "member";
+    try {
+    await addDoc(collection(db, "users"), userData);
+     callback({
+      status: "success",
+      message: "User registered successfully",
+     });
+    } catch (error: any) {
+     callback({
+      status: "error",
+      message: error.message,
+  });
+  }
+}
 }
--- praktikum14/my-app/src/views/auth/register/index.tsx
+++ praktikum14/my-app/src/views/auth/register/index.tsx
@@ -9,12 +9,28 @@ const TampilanRegister = () => {
   const [error, setError] = useState("");
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setError("");
+    setIsLoading(true);
+
     event.preventDefault();
     const form = event.currentTarget;
     const formdata = new FormData(event.currentTarget);
     const email = formdata.get("email") as string;
     const fullName = formdata.get("Fullname") as string;
     const password = formdata.get("Password") as string;
+
+    if (!email) {
+      setError("Email wajib diisi");
+      setIsLoading(false);
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Password minimal 6 karakter");
+      setIsLoading(false);
+      return;
+    }
+
     const response = await fetch("/api/register", {
       method: "POST",
       headers: {
@@ -33,22 +49,23 @@ const TampilanRegister = () => {
       setIsLoading(false);
       setError(
         response.status === 400
-          ? "User already exists"
+          ? "Email already exists"
           : "An error occurred. Please try again later."
       );
     }
   };
+
   return (
     <div className={style.register}>
+          {error && <p className={style.register__error}>{error}</p>}
       <h1 className={style.register__title}>Halaman Register</h1>
       <div className={style.register__form}>
-        <form action="" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div className={style.register__form__item}>
             <label
               htmlFor="email"
               className={style.register__form__item__label}
-            >
-              Email
+            > Email
             </label>
             <input
               type="email"
@@ -63,8 +80,7 @@ const TampilanRegister = () => {
             <label
               htmlFor="Fullname"
               className={style.register__form__item__label}
-            >
-              Fullname
+            > Fullname
             </label>
             <input
               type="text"
@@ -79,8 +95,7 @@ const TampilanRegister = () => {
             <label
               htmlFor="Password"
               className={style.register__form__item__label}
-            >
-              Password
+            >Password
             </label>
             <input
               type="password"
@@ -90,8 +105,9 @@ const TampilanRegister = () => {
               className={style.register__form__item__input}
             />
           </div>
-          <button type="submit" className={style.register__form__item__button}>
-            Register
+          <button type="submit" className={style.register__form__item__button}
+            disabled={isLoading}>
+            {isLoading ? "Loading..." : "Register"}
           </button>
         </form>
         <br />
