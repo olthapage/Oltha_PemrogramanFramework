6dd1ca80da9c902dcaee784f2fa2a5a7b0e811fd
olthapage <olthar.rosyeda01@gmail.com>
2026-04-02 16:48:07 +0700

    JS15_praktikum_bagian1

--- praktikum14/my-app/src/pages/auth/register.tsx
+++ praktikum14/my-app/src/pages/auth/register.tsx
@@ -1,5 +1,9 @@
 import RegisterView from "../../views/auth/register";
 
-export default function RegisterPage() {
-  return <RegisterView />;
-}
+const halamanRegister = () => {
+  return (
+    <RegisterView />
+  );
+};
+
+export default halamanRegister;
--- praktikum14/my-app/src/views/auth/register/index.tsx
+++ praktikum14/my-app/src/views/auth/register/index.tsx
@@ -1,17 +1,70 @@
-import styles from "./register.module.css";
+import Link from "next/link";
+import style from "./register.module.scss";
 
-export default function RegisterView() {
+const TampilanRegister = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Halaman Register</h1>
+    <div className={style.register}>
+      <h1 className={style.register__title}>Halaman Register</h1>
+      <div className={style.register__form}>
+        <form action="">
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="email"
+              className={style.register__form__item__label}
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              className={style.register__form__item__input}
+            />
+          </div>
 
-      <form className={styles.form}>
-        <input type="text" placeholder="Nama" className={styles.input} />
-        <input type="email" placeholder="Email" className={styles.input} />
-        <input type="password" placeholder="Password" className={styles.input} />
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Fullname"
+              className={style.register__form__item__label}
+            >
+              Fullname
+            </label>
+            <input
+              type="text"
+              id="Fullname"
+              name="Fullname"
+              placeholder="Fullname"
+              className={style.register__form__item__input}
+            />
+          </div>
 
-        <button className={styles.button}>Daftar</button>
-      </form>
+          <div className={style.register__form__item}>
+            <label
+              htmlFor="Password"
+              className={style.register__form__item__label}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="Password"
+              name="Password"
+              placeholder="Password"
+              className={style.register__form__item__input}
+            />
+          </div>
+          <button type="submit" className={style.register__form__item__button}>
+            Register
+          </button>
+        </form>
+        <br />
+        <p className={style.register__form__item__text}>
+          Sudah punya akun? <Link href="/auth/login">Ke Halaman Login</Link>
+        </p>
+      </div>
     </div>
   );
-}
+};
+
+export default TampilanRegister;
