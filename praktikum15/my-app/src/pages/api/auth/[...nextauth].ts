f5d4989a02d5afc722805032a13ce9dea2070734
olthapage <olthar.rosyeda01@gmail.com>
2026-04-03 14:51:14 +0700

    BAGIAN 3 – Authorize di NextAuth (Database Login)

--- praktikum15/my-app/src/pages/api/auth/[...nextauth].ts
+++ praktikum15/my-app/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { signIn } from "@/utils/db/servicefirebase";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -10,48 +12,33 @@ export const authOptions: NextAuthOptions = {
     CredentialsProvider({
       name: "credentials",
       credentials: {
-        fullname: { label: "Full Name", type: "text" },
+        // fullname: { label: "Full Name", type: "text" },
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials ) {
-        const user: any = {
-          id: "1",
-          email: credentials?.email,
-          password: credentials?.password,
-          fullname: credentials?.fullname,
-        };
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user: any = await signIn(credentials.email);
+
         if (user) {
-          return user;
-        } else {
-          return null;
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+
+          if (isPasswordValid) {
+            // Pastikan mengembalikan object user yang bersih
+            return {
+              id: user.id,
+              email: user.email,
+              fullname: user.fullname,
+              role: user.role,
+            };
+          }
         }
+        return null;
       },
     }),
   ],
-
-  callbacks: {
-    async jwt({ token, account, profile, user }: any) {
-      if (account?.provider === "credentials" && user) {
-        token.email = user.email;
-        token.fullname = user.fullname;
-      }
-      return token;
-    },
-    async session({ session, token }: any) {
-      if (token.email) {
-        session.user.email = token.email;
-      }
-      if (token.fullname) {
-        session.user.fullname = token.fullname;
-      }
-      return session;
-    },
-  },
-
-  pages: {
-  signIn: "/auth/login",
-  },
 };
-
-export default NextAuth(authOptions);
