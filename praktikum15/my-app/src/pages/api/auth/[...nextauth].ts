42fc633494c645535a6a560f4ad5f0113037bb4b
olthapage <olthar.rosyeda01@gmail.com>
2026-04-03 14:59:03 +0700

    BAGIAN 4 – Tambahkan Role ke Token

--- praktikum15/my-app/src/pages/api/auth/[...nextauth].ts
+++ praktikum15/my-app/src/pages/api/auth/[...nextauth].ts
@@ -41,4 +41,37 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+
+  // TAMBAHAN SESUAI PERINTAH DI GAMBAR
+  callbacks: {
+    async jwt({ token, account, profile, user }: any) {
+      if (account?.provider === "credentials" && user) {
+        token.email = user.email;
+        token.fullname = user.fullname;
+        token.role = user.role;
+      }
+
+      // console.log("jwt callback", { token, account, profile, user })
+      return token;
+    },
+
+    async session({ session, token }: any) {
+      if (token.email) {
+        session.user.email = token.email;
+      }
+
+      if (token.fullname) {
+        session.user.fullname = token.fullname;
+      }
+
+      if (token.role) {
+        session.user.role = token.role;
+      }
+
+      // console.log("session callback", { session, token })
+      return session;
+    },
+  },
 };
+
+export default NextAuth(authOptions);
