552a2125f097cb0fb723dcbe630e7e69bf36ded6
olthapage <olthar.rosyeda01@gmail.com>
2026-04-03 15:05:32 +0700

    BAGIAN 5 – Callback URL Logic

--- praktikum15/my-app/src/Middleware/withAuth.ts
+++ praktikum15/my-app/src/Middleware/withAuth.ts
@@ -15,8 +15,9 @@ export default function withAuth(
       });
 
       if (!token) {
-        const loginUrl = new URL("/auth/login", req.url);
-        return NextResponse.redirect(loginUrl);
+        const Url = new URL("/auth/login", req.url);
+        Url.searchParams.set("callbackUrl", encodeURI(req.url));
+        return NextResponse.redirect(Url);
       }
     }
     return middleware(req, next);
