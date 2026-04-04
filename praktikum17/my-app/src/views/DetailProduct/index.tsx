1c222101d071d14015a16bf86b5bcf737485fac6
olthapage <olthar.rosyeda01@gmail.com>
2026-04-04 15:01:21 +0700

    Tugas Praktikum

--- praktikum17/my-app/src/views/DetailProduct/index.tsx
+++ praktikum17/my-app/src/views/DetailProduct/index.tsx
@@ -1,25 +1,36 @@
 import { ProductType } from "../../types/product.type";
 import styles from "@/views/DetailProduct/detailProduct.module.scss";
+import Image from "next/image";
 
 const DetailProduk = ({ products }: { products: ProductType }) => {
-    return (
-      <>
-        <h1 className={styles.produkDetail__title}>Detail Produk</h1>
-        <div className={styles.produkDetail}>
-          <div className={styles.produkDetail__image}>
-            <img src={products.image && products.image} alt={products.name} />
-          </div>
+  return (
+    <>
+      <h1 className={styles.produkDetail__title}>Detail Produk</h1>
 
-          <div className={styles.produkDetail__info}>
-            <h1 className={styles.produkDetail__name}>{products.name}</h1>
-            <p className={styles.produkDetail__category}>{products.category}</p>
-            <p className={styles.produkDetail__price}>
-              Rp {products.price && products.price.toLocaleString("id-ID")}
-            </p>
-          </div>
+      <div className={styles.produkDetail}>
+        <div className={styles.produkDetail__image}>
+          <Image
+            src={products.image}
+            alt={products.name}
+            width={400}
+            height={400}
+          />
         </div>
-      </>
-    );
+
+        <div className={styles.produkDetail__info}>
+          <h1 className={styles.produkDetail__name}>{products.name}</h1>
+
+          <p className={styles.produkDetail__category}>
+            {products.category}
+          </p>
+
+          <p className={styles.produkDetail__price}>
+            Rp {products.price.toLocaleString("id-ID")}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DetailProduk;
