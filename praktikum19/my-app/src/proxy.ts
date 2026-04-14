06a8249b73ae4b30805eec5ac97ce084a8897d49
olthapage <olthar.rosyeda01@gmail.com>
2026-04-14 20:16:08 +0700

    feat: add device list UI

--- praktikum19/my-app/src/proxy.ts
+++ praktikum19/my-app/src/proxy.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest, NextFetchEvent } from "next/server";
 import withAuth from "./Middleware/withAuth";
-function middleware(request: NextRequest, event: NextFetchEvent) {
+function proxy(request: NextRequest, event: NextFetchEvent) {
   return NextResponse.next();
 }
 
-export default withAuth(middleware, ["/profile", "/produk", "/about", "/admin"]);
+export default withAuth(proxy, ["/profile", "/produk", "/about", "/admin"]);
 
 export const config = {  
   matcher: ["/profile", "/produk", "/about", "/admin"],
